7904fa03919abf693fe7c6abb2a2802072f773f8
Mike Hickey <136211203+mikehickey2@users.noreply.github.com>
2025-09-23 20:09:57 -0400

    "NLP code partial fail-created test file"

--- scripts/2_FAA _data_clean.r
+++ scripts/2_FAA _data_clean.r
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(dplyr)
 
+#This code below worked for the first observation; and 110 others. Need to fix.
 incursion_data_extracted <- incursion_data %>%
   
   mutate(
@@ -21,3 +22,24 @@ incursion_data_extracted <- incursion_data %>%
     
     report_type = str_extract(Summary, "PRELIM INFO|FINAL INFO")
   )
+
+
+#created test file to isolate NA Rows to help w/ imrpoving NLP above.  
+
+# Use is.na() on the selected columns and rowSums() to find rows with NAs
+na_rows <- is.na(incursion_data_extracted$time) |
+  is.na(incursion_data_extracted$altitude) |
+  is.na(incursion_data_extracted$reporter) |
+  is.na(incursion_data_extracted$relative_location) |
+  is.na(incursion_data_extracted$evasive_action) |
+  is.na(incursion_data_extracted$disposition) |
+  is.na(incursion_data_extracted$report_type)
+
+# Create a new data frame with only the rows that contain at least one NA
+NA_df <- incursion_data_extracted[na_rows, ]
+
+# Write the new data frame to a CSV file in the "output" folder
+write.csv(NA_df, file = "output/NA_df.csv", row.names = FALSE)
+
+# Print a message to confirm the file has been created
+message("NA_df.csv has been created in the 'output' directory.")
